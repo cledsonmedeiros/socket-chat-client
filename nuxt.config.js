796bf1dfa5215fbecda924a64c86201bdefdd0eb
Cledson Medeiros <cledson.lmedeiros@hotmail.com>
2022-03-13 23:43:40 -0300

    change to ssr

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,8 +1,9 @@
 import colors from "vuetify/es5/util/colors";
 
 export default {
-  ssr: false,
-  target: "static",
+  ssr: true,
+
+  target: "server",
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   env: {
